b1017a73f8cc0d604645d5973424ac4096c14439
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2025-03-31 16:53:28 +0200

    add company name, tax domicile, and NIF fields to CustomerEntity

--- src/domain/customer/customer.entity.ts
+++ src/domain/customer/customer.entity.ts
@@ -2,6 +2,9 @@ import { Name } from './name';
 import { Surname } from './surname';
 import { CustomerId } from './id';
 import { Email } from './email';
+import { CompanyName } from './companyName';
+import { TaxDomicile } from './taxDomicile';
+import { NIF } from './nif';
 
 export class CustomerEntity {
   private constructor(
@@ -9,6 +12,9 @@ export class CustomerEntity {
     readonly name: Name,
     readonly surname: Surname,
     readonly email: Email,
+    readonly companyName: CompanyName,
+    readonly taxDomicile: TaxDomicile,
+    readonly NIF: NIF,
   ) {}
 
   static create(
@@ -16,12 +22,26 @@ export class CustomerEntity {
     aName: string,
     aSurname: string,
     anEmail: string,
+    aCompanyName: string,
+    aTaxDomicile: string,
+    aNIF: string,
   ): CustomerEntity {
     const id = CustomerId.create(anId);
     const name = Name.create(aName);
     const surname = Surname.create(aSurname);
     const email = Email.create(anEmail);
+    const companyName = CompanyName.create(aCompanyName);
+    const taxDomicile = TaxDomicile.create(aTaxDomicile);
+    const nif = NIF.create(aNIF);
 
-    return new CustomerEntity(id, name, surname, email);
+    return new CustomerEntity(
+      id,
+      name,
+      surname,
+      email,
+      companyName,
+      taxDomicile,
+      nif,
+    );
   }
 }
