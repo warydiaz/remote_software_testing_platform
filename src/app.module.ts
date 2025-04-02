f65ed65b9c6e103d46bf4c6e04b8497aee740e89
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2025-04-02 16:55:22 +0200

    register CustomerPersistenceEntity in TypeOrmModule; remove unnecessary eslint-disable comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -6,17 +9,16 @@ import { CreateCustomerController } from './core/ui/api/create-customer.controll
 import { RegisterCustomerCommandHandler } from './core/application/customer/register-customer.command-handler';
 import { CUSTOMER_REPOSITORY } from './core/domain/customer/customer.repository';
 import { CustomerTypeOrmRepository } from './core/infrastructure/postgres/customer-repository';
-
+import { CustomerPersistenceEntity } from './core/infrastructure/postgres/entities/customer.persistence.entity';
 @Module({
   imports: [
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     ConfigModule.forRoot(), // Cargar variables de entorno
     TypeOrmModule.forRoot(typeOrmConfig), // Importar la configuración de TypeORM
+    TypeOrmModule.forFeature([CustomerPersistenceEntity]), // 👈 Registrar la entidad en TypeORM
   ],
   controllers: [CreateCustomerController],
   providers: [
     RegisterCustomerCommandHandler,
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     { provide: CUSTOMER_REPOSITORY, useClass: CustomerTypeOrmRepository },
   ],
 })
